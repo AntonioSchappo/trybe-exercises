b29083d6a1245163bcedd1e57af791dae1c27393
Antônio Schappo <antonioschappo@gmail.com>
2020-10-27 18:52:02 -0300

    Done Exercise 1 of 4.1

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio_4_1.js
@@ -0,0 +1,14 @@
+/* Exercício 1 */
+let a = 4;
+let b = 2;
+
+console.log(a+b);
+console.log(b-a);
+console.log(a*b);
+console.log(a/b);
+if (a%b==0){
+console.log("Even");
+};
+
+/* Exercício 2 */
+
