7ad98bb38ab2b3d95d8973885d6f753baf8b648a
Antônio Schappo <antonioschappo@gmail.com>
2020-10-27 15:44:13 -0300

    Starting 4.2 exercises

---
+++ introducao-a-javascript-e-logica-de-programacao_2/ambiente.js
@@ -0,0 +1,8 @@
+let num = [0,5,8,3,2,1];
+/*for(let pos = 0; pos<num.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}.`)
+}*/
+num.sort();
+for(let pos in num){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}.`)
+}   
