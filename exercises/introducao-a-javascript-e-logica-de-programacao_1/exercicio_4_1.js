b8901b4d4665838abde15e5d8854922619c939d7
Antônio Schappo <antonioschappo@gmail.com>
2020-10-27 18:57:04 -0300

    Done Exercise 2 of 4.1

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio_4_1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio_4_1.js
@@ -12,3 +12,15 @@ console.log("Even");
 
 /* Exercício 2 */
 
+let num1 = 18;
+let num2 = 9;
+if(num1>num2){
+    console.log(`O maior número é o ${num1}!`)
+}else if(num2>num1){
+    console.log(`O maior número é o ${num2}!`)
+}else{
+    console.log('Erro! Não foi possível identificar o maior número!')
+};
+
+/* Exercício 3 */
+
