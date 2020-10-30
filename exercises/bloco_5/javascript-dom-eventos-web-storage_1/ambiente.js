f3c5aa3ca1bee4ed9c6c183b5f769a1eab2067a9
Antônio Schappo <antonioschappo@gmail.com>
2020-10-30 20:07:26 -0300

    Add style to pre-class exercises

---
+++ exercises/bloco_5/javascript-dom-eventos-web-storage_1/ambiente.js
@@ -0,0 +1,7 @@
+document.getElementById("page-title").innerText = "Children of Men";
+var paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+document.getElementById("second-paragraph").innerText = "Lorem ipsum...";
+document.getElementById("subtitle").innerText = "A volta dos que não foram.";
+document.getElementsByClassName("styleparag")[0].style.backgroundColor = 'white';
+document.getElementsByTagName("h4")[0].innerText = 'Mudei a tag, porra'
