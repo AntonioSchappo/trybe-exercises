66a92d8684b6e21dff6103c77b54028a01c6976f
Antônio Schappo <antonioschappo@gmail.com>
2020-11-02 20:42:05 -0300

    All required done, need to do bonus ones

--- introducao-a-javascript-e-logica-de-programacao_2/exercicios4_2.js
+++ introducao-a-javascript-e-logica-de-programacao_2/exercicios4_2.js
@@ -37,3 +37,37 @@ for (const i in numbers) {
 console.log(largest);
 
 //6. Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+let impares = [];
+for (const i in numbers) {
+    if (numbers[i]%2 !== 0) {
+    impares.push(numbers[i]);       
+    }
+};
+if(impares.length < 0){
+console.log("nenhum valor ímpar encontrado")
+}else{
+console.log(`O array possui ${impares.length} números ímpares.`)
+}
+
+//7. Utilizando for, descubra qual o menor valor contido no array e imprima-o;
+let smallest = 1000;
+for (const i in numbers) {
+    if(numbers[i] < smallest){
+        smallest = numbers[i];
+    }
+}
+console.log(smallest);
+
+//8. Utilizando for, crie uma array que vá de 1 até 25 e imprima o resultado;
+let array25 = [];
+for (let i = 1; i <= 25; i+=1) {
+    array25.push(i); 
+}
+console.log(array25);
+
+//9. Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2.
+let array12AndaHalf = [];
+for(let i = 0; i < array25.length; i+=1){
+    array12AndaHalf.push(array25[i]/2)
+};
+console.log(array12AndaHalf);
